8701f7e781b06ca73c3e64588d2f54e06c710e12
Bartosz <bartoszchmielecki2003@gmail.com>
2025-01-26 23:08:20 +0100

    Day 138

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,11 +1,42 @@
 const fs = require("fs");
+const http = require("http");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+//////////////////
 
-console.log(textIn);
+// FILES
 
-const textOut = `This is what we know about the avocados: ${textIn}.\n Created on ${Date.now()}`;
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocados: ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log(textOut);
 
-fs.writeFileSync("./txt/output.txt", textOut);
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-console.log(textOut);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log(`Your file has be written`);
+//       });
+//     });
+//   });
+// });
+
+// console.log("Will read file!");
+
+////////////////////
+
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
