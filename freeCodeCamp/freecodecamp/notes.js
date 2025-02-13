e55304fffa3c60faf58cbcd84fd515df90b7ea7b
Bartosz <bartoszchmielecki2003@gmail.com>
2025-02-13 10:47:52 +0100

    !

--- freeCodeCamp/freecodecamp/notes.js
+++ freeCodeCamp/freecodecamp/notes.js
@@ -3,8 +3,37 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// 13.02
+
+// We can't use .env data when file is named for eg mongo.env
+
+// require("dotenv").config({ path: __dirname + "mongo.env" });
+
+require("dotenv").config();
+// require("dotenv").config({ path: __dirname + "/mongo.env" });
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//bodyParser if to use req.body it gets the data and can be used with post and of course parses it
+
+/*
+
+extended is a configuration option that tells body-parser which parsing needs to be used.
+When extended=false it uses the classic encoding querystring library.
+When extended=true it uses qs library for parsing.
+
+When using extended=false, values can be only strings or arrays.
+The object returned when using querystring does not prototypically inherit from the default JavaScript Object, which means functions like hasOwnProperty, toString will not be available.
+The extended version allows more data flexibility, but it is outmatched by JSON.
+
+*/
+app.use("/", express.static(path.join(__dirname + "/public")));
+
+// 12.02
+
 //Module that loads enviroment variables from .env file intro process.env
 require("dotenv").config();
+// require("dotenv").config({ path: __dirname + "/mongo.env" });
 
 /*
 To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
@@ -38,3 +67,12 @@ app.get("/json", (req, res) => {
 
 // Starting server
 app.listen(3000, (req, res) => {});
+
+// this is setting params for eg word is a param
+/*
+route_path: '/user/:userId/book/:bookId'
+actual_request_URL: '/user/546/book/6754'
+req.params: {userId: '546', bookId: '6754'}
+
+This is info for egzample that someone wants to fetch from our api route_path is a param route parameter
+*/
--- freeCodeCamp/freecodecamp/server.js
+++ freeCodeCamp/freecodecamp/server.js
@@ -2,35 +2,93 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+// to use post and body we need ither rxpress.json() or body parser
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//
+app.use(express.static(path.join(__dirname + "/public")));
 require("dotenv").config();
 
-app.use("/", express.static(path.join(__dirname + "/public")));
+//If you switch intertnet or wehre you are you have switch ip in mongodb atlas.
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) => {
+    console.log(`Connected, ${res}`);
+  })
+  .catch((err) => {
+    console.log(`Unable to connect: ${err}`);
+  });
 
-app.use(
+//api
+
+app.route("/api/:date").get((req, res) => {
+  const dateType = req.params.date;
+  let parsedDate;
+
+  // Check if the input contains a hyphen (for "YYYY-MM-DD" format)
+  if (dateType.includes("-")) {
+    parsedDate = new Date(dateType);
+  }
+  // Check if the input is a valid UNIX timestamp (number only)
+  else {
+    parsedDate = new Date(Number(dateType));
+  }
+
+  // Check if the date is invalid (NaN)
+  if (isNaN(parsedDate.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString(),
+  });
+});
+
+app
+  .route("/karina/:id")
+  .get((req, res) => {
+    console.log("Received POST request!");
+    console.log("Request Body:", req.body);
+    const loveInd = req.query.love;
+    const loveType = req.query.type;
+    const idType = req.params.id;
+
+    res.send(
+      `Karina loves me ${loveInd} and in type of ${loveType}. Love id: ${idType}`
+    );
+  })
+  .post((req, res) => {
+    const loveInd = req.body.loveInd;
+    const loveType = req.body.loveType;
+    const idType = req.params.id;
+    console.log("Received POST request!");
+    console.log("Request Body:", req.body);
+    res.json({
+      message: "Data received successfully!",
+      loveInd: loveInd,
+      loveType: loveType,
+      idType: idType,
+    });
+  });
+
+app.get(
   "/app",
   (req, res, next) => {
     req.time = new Date().toString();
-    req.hm = req.params;
 
     next();
   },
   (req, res) => {
-    res.send({
+    res.json({
       "Cur Time": req.time,
     });
-    console.log(req.hm);
   }
 );
 
-// this is setting params for eg word is a param
-/*
-route_path: '/user/:userId/book/:bookId'
-actual_request_URL: '/user/546/book/6754'
-req.params: {userId: '546', bookId: '6754'}
-
-This is info for egzample that someone wants to fetch from our api route_path is a param route parameter
-*/
 app.get("/:word/echo", (req, res) => {
   const { word } = req.params;
 
@@ -38,7 +96,7 @@ app.get("/:word/echo", (req, res) => {
     echo: word,
   });
 });
-app.get("/app", (req, res) => {});
+
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
@@ -51,4 +109,6 @@ app.get("/json", (req, res) => {
   }
 });
 
-app.listen(3000, (req, res) => {});
+app.listen(3000, (req, res) => {
+  console.log("Server is runing at port 3000");
+});
